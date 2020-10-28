e79d37c9b1edb722738502c9a538e746e840af66
Bruce McClure <brucemcc90@gmail.com>
2020-10-28 18:54:08 +1100

    test

--- src/swagger.py
+++ src/swagger.py
@@ -1,11 +1,8 @@
-from flask import Flask
-import swagger
 from flask_swagger_ui import get_swaggerui_blueprint
 
-app = Flask(__name__)
 
 SWAGGER_URL = '/swagger'
-API_URL = '/static'
+API_URL = '/static/swagger.json'
 
 swaggerui_blueprint = get_swaggerui_blueprint(
     SWAGGER_URL, 
@@ -14,10 +11,3 @@ swaggerui_blueprint = get_swaggerui_blueprint(
         'app_name': "Bruces-python-Flask-rest-biolerplate"
     }
 )
-
-
-app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL) 
-
-@app.route("/")
-def home_page():
-    return "This is my homepage! Pretty cool."
