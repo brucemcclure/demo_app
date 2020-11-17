d49ddf2f0f09649d7c591ebca6439d8555f4c44c
Bruce McClure <brucemcc90@gmail.com>
2020-11-17 12:45:49 +1100

    basic user schema

---
+++ src/schemas/UserSchema.py
@@ -0,0 +1,14 @@
+from main import ma                                                 # Import the serialization object from main
+from models.User import User                                        # Importign the User model
+from marshmallow.validate import Length                             # Import the length class that will allow us to validate the length of the string 
+
+class UserSchema(ma.SQLAlchemyAutoSchema):                          # Generates Schema automatically
+    class Meta:
+        model = User                                                # Generate Schema using the User Model
+
+    email = ma.String(required=True, validate=Length(min=4))        # The email is required and must be at least 6 chars long
+    # password = ma.String(required=True, validate=Length(min=6))
+
+
+user_schema = UserSchema()                                          # Schema for single users
+users_schema = UserSchema(many=True)                                # Schema for multiple users    
