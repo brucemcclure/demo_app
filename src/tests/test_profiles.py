5de726797ccdba29b6d58120b8a6a8dd770e8b2f
Bruce McClure <brucemcc90@gmail.com>
2020-11-19 07:01:49 +1100

    tear down method for profile created and docced

--- src/tests/test_profiles.py
+++ src/tests/test_profiles.py
@@ -8,7 +8,7 @@ from models.Profile import Profile                                         # The
 
 class TestProfiles(unittest.TestCase):                                      # This is the Parent class that will test our Profile functionality.    
         @classmethod
-        def setUp(cls):
+        def setUp(cls):                                                     # This method will run before each and every class
             cls.app = create_app()                                          # Create a new instance of app
             cls.app_context = cls.app.get_app_context()                     # Creating context for which the app is in. The tests run in parrallel therefore we need to keep track of which instance of app we are using
             cls.app_context.push()                                          # Pushing context. Read the docs for more
@@ -16,3 +16,9 @@ class TestProfiles(unittest.TestCase):                                      # Th
             db.create_all()                                                 # Create all the 
             runner = cls.app.test_cli_runner()
             runner.invoke(args=["db", "seed"])                              # This seeds the db
+
+        @classmethod                                                        # This method will run after each and every class
+         def tearDown(cls):                                                 # We want to delete all the data from the class tests
+             db.session.remove()                                            # Remove the session from the db
+             db.drop_all()                                                  # Drop all tables
+             cls.app_context.pop()                                          # Remove the context of the app
