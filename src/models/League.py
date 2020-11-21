31fbda3e8c58a06f8451a28baf4ab66acaedaa8f
Bruce McClure <brucemcc90@gmail.com>
2020-11-21 16:12:58 +1100

    doccing the League Model

--- src/models/League.py
+++ src/models/League.py
@@ -3,9 +3,9 @@ from main import db                                                    # This is
 class League(db.Model):                                                 # Creating a League class inheriting from db.Model            
     __tablename__= "leagues"                                            # Explicitally providing the name of the table             
 
-    id = db.Column(db.Integer, primary_key=True)                                     
-    title = db.Column(db.String(), nullable=False, unique=True)                       
-    description = db.Column(db.String(), nullable=False)                            
+    id = db.Column(db.Integer, primary_key=True)                        # Creates a column called id and sets it al the primary key             
+    title = db.Column(db.String(), nullable=False, unique=True)         # Title, string, must be present, must be unique              
+    description = db.Column(db.String(), nullable=False)                # Description, string, must be present, must be unique            
 
-    def __repr__(self):                                                           
-        return f"<Title {self.title}>"
+    def __repr__(self):                                                 # Reresentitive state          
+        return f"<Title {self.title}>"                                  # When the League is printed it now shows the title instead of the id
