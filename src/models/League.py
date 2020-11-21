c13a89ad76346747817250124a9aebf3fa3e852a
Bruce McClure <brucemcc90@gmail.com>
2020-11-21 16:11:33 +1100

    doccing the League Model

--- src/models/League.py
+++ src/models/League.py
@@ -1,7 +1,7 @@
-from main import db                                                                   
+from main import db                                                    # This is the db instance created by SQLAlchemy                                                                   
 
-class League(db.Model):                                                             
-    __tablename__= "leagues"                                                         
+class League(db.Model):                                                 # Creating a League class inheriting from db.Model            
+    __tablename__= "leagues"                                            # Explicitally providing the name of the table             
 
     id = db.Column(db.Integer, primary_key=True)                                     
     title = db.Column(db.String(), nullable=False, unique=True)                       
