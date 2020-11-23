73b505158db04b9bcf12e9346b012ecc58b73ce4
Bruce McClure <brucemcc90@gmail.com>
2020-11-23 16:19:02 +1100

    added leagues_categories and linked Category to League

--- src/models/League.py
+++ src/models/League.py
@@ -1,4 +1,12 @@
-from main import db                                                    # This is the db instance created by SQLAlchemy                                                                   
+from main import db     
+from models.Category import Category
+from sqlalchemy.orm import backref                       
+
+leagues_categories = db.Table( "leagues_categories"
+    db.Column('league_id', db.Integer, db.ForeignKey('leagues.id')),
+    db.Column('category_id', db.Integer, db.ForeignKey('categories.id'))
+
+)
 
 class League(db.Model):                                                 # Creating a League class inheriting from db.Model            
     __tablename__= "leagues"                                            # Explicitally providing the name of the table             
@@ -7,6 +15,7 @@ class League(db.Model):                                                 # Creati
     title = db.Column(db.String(), nullable=False, unique=True)         # Title, string, must be present, must be unique              
     description = db.Column(db.String(), nullable=False)                # Description, string, must be present, must be unique            
     owner = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)       # user_id is an integer and the Foreign key comes from the users table id. It is required
-
+    leagues_categories = db.relationship('Category', secondary=leagues_categories, backref=db.backref('leagues_categories', lazy = 'dynamic'))
+    
     def __repr__(self):                                                 # Reresentitive state          
         return f"<Title {self.title}>"                                  # When the League is printed it now shows the title instead of the id
