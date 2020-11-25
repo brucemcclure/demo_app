24799bbedb4ecabb647064907ef7d2c0967e7d71
Bruce McClure <brucemcc90@gmail.com>
2020-11-25 14:32:28 +1100

    created Sprint Schema

---
+++ src/schemas/SprintSchema.py
@@ -0,0 +1,16 @@
+from main import ma                                                   
+from models.Sprint import Sprint                                      
+from marshmallow.validate import Length                               
+from schemas.UserSchema import UserSchema       
+
+
+class SprintSchema(ma.SQLAlchemyAutoSchema):                          
+    class Meta:
+        model = Sprint                                               
+
+    title = ma.String(required=True, validate=Length(min=1))          
+    meeting_point = ma.String(required=True, validate=Length(min=1))    
+    creation_time = ma.DateTime(required=True)    
+    
+sprint_schema = SprintSchema()                                        
+sprints_schema = SprintSchema(many=True)                              
