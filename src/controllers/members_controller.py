1c508e12a5412d82789b2ae1f0d0487cb8ac769a
Bruce McClure <brucemcc90@gmail.com>
2020-11-28 15:16:45 +1100

    created member controller

---
+++ src/controllers/members_controller.py
@@ -0,0 +1,28 @@
+from models.League import League                                       # Importing the League Model
+from models.User import User                                           # Importing the User Model
+from models.Member import Member
+from schemas.MemberSchema import member_schema, members_schema
+from main import db                                                    # This is the db instance created by SQLAlchemy
+from services.auth_service import verify_user 
+from flask_jwt_extended import jwt_required, get_jwt_identity          # Packages for authorization via JWTs
+from flask import Blueprint, request, jsonify, abort                   # Import flask and various sub packages
+
+members = Blueprint("members", __name__, url_prefix="/league")         # Creating the league blueprint 
+
+
+
+@members.route("/<int:id>/members", methods=["POST"])   
+@jwt_required 
+@verify_user    
+def league_add_members(user, id):   
+    league = League.query.filter_by(id=id, owner=user.id).first() 
+    data = request.json
+    for user_id in data["members"]:
+        print(user_id)
+        new_member = Member() 
+        new_member.user_id = user_id
+        new_member.league_id = id
+        db.session.add(new_member) 
+    db.session.commit() 
+
+    return(f"The members were successfully added to the league")
