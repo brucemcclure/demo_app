9085c802b35ad920ba75b70208940d14b14e1e13
Bruce McClure <brucemcc90@gmail.com>
2020-11-29 12:10:35 +1100

    added active to members

--- src/models/Member.py
+++ src/models/Member.py
@@ -7,6 +7,7 @@ class Member(db.Model):
     id = db.Column(db.Integer, primary_key=True)  
     user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
     league_id = db.Column(db.Integer, db.ForeignKey("leagues.id"), nullable=False)
+    active = db.Column(db.Boolean(), nullable=False)
     # giver = db.relationship("Point", backref=backref("point")) 
     # receiver = db.relationship("Point", backref=backref("point")) 
     def __repr__(self):                                                               # When printing the model we will see its email attribute
